e9c9048aefcd6f853409cf12cd98cb39385b34f8
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-06-22 23:40:21 -0300

    chore: structure of frontend

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import '../styles/globals.css'
+import Head from 'next/head';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>OneBitGames</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
