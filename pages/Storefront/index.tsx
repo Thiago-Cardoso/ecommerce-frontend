4e3317c3165e1a3738b18bb9b793c2a14fb1c3c8
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-06-23 20:01:54 -0300

    feat: create component MainComponent

---
+++ pages/Storefront/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainComponent from '../../components/shared/MainComponent';
+
+const Storefront: React.FC = () => {
+  return (
+    <>
+      <MainComponent>
+        <h1>Store front</h1>
+      </MainComponent>
+    </>
+  )
+}
+
+export default Storefront;
