f515792595e8fc2130dcd6588f9410e065355bf4
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-06-23 23:55:42 -0300

    chore: configuration of reducer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,23 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../store';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <title>OneBitGames</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Head>
+            <title>OneBitGames</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          </Head>
 
-      <Component {...pageProps} />
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
     </>
   )
 }
