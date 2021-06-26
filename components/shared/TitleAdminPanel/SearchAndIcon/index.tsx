1d14ff3709c821c0b4032b9e2e59bef53470dde1
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-06-26 18:58:51 -0300

    feat: create TitleAdminPanel,SearchAndIcon and TitleAndPath component

---
+++ components/shared/TitleAdminPanel/SearchAndIcon/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { InputGroup, FormControl, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import styles from '../../../../styles/AdminTitle.module.css';
+import StyledButton from '../../StyledButton';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+
+interface SearchAndIcon {
+    icon: IconProp
+}
+
+const SearchAndIcon: React.FC<SearchAndIcon> = ({icon}) => {
+    return (
+        <Col lg={9} xs>
+            <Row>
+                <Col lg={9} xs={10}>
+                    <InputGroup>
+                        <FormControl placeholder="Pesquisar" className={styles.input} />
+                    </InputGroup>
+                </Col>
+
+                <Col lg={3} xs={2} className="mt-1" style={{ cursor: 'pointer' }}>
+                    <FontAwesomeIcon icon={faSearch} size="lg" color="var(--color-gray-light)" className="float-left" />
+                </Col>
+
+                <Col lg={2} xs={{span: 3}} className={styles.titleButton}>
+                    <StyledButton icon={icon} type_button="blue" />
+                </Col>
+            </Row>
+        </Col>
+    )
+}
+
+export default SearchAndIcon;
