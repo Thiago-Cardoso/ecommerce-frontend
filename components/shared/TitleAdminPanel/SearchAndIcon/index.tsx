8d643f93ab329ca90e09f386ac4b7f1997d26559
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-07-06 23:33:45 -0300

    feat: list of category

--- components/shared/TitleAdminPanel/SearchAndIcon/index.tsx
+++ components/shared/TitleAdminPanel/SearchAndIcon/index.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import styles from '../../../../styles/AdminTitle.module.css';
 import { InputGroup, FormControl, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import styles from '../../../../styles/AdminTitle.module.css';
 import StyledButton from '../../StyledButton';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import Link from 'next/link';
 
 import { useDispatch } from 'react-redux';
@@ -20,7 +20,7 @@ interface SearchAndIcon {
     newPath: string;
 }
 
-const SearchAndIcon: React.FC<SearchAndIcon> = ({icon, newPath }) => {
+const SearchAndIcon: React.FC<SearchAndIcon> = ({icon, newPath}) => {
     const [search, setSearch] = useState('');
     const dispatch = useDispatch();
     const router = useRouter();
@@ -29,54 +29,57 @@ const SearchAndIcon: React.FC<SearchAndIcon> = ({icon, newPath }) => {
         dispatch(clearSearch());
     }, [])
 
-    const handleSearch = () : void => {
+    const handleSearch = (): void => {
         router.replace(router.pathname, '?page=1');
         dispatch(setSearchRedux(search));
     }
 
     return (
-        <Col lg={9} xs>
-            <Row>
-                <Col lg={9} xs={10}>
-                    <InputGroup>
-                        <FormControl 
-                            placeholder="Pesquisar"
-                            className={styles.input}
-                            value={search}
-                            onChange={
-                                (evt: React.ChangeEvent<HTMLInputElement>) =>
-                                setSearch(evt.target.value)
-                            }
-                            onKeyPress= {
-                                (evt: React.KeyboardEvent<HTMLInputElement>) => {
-                                    if(evt.key.toLocaleUpperCase() === 'enter') {
-                                        handleSearch();
+        <Row>
+            <Col lg={9} xs>
+                <Row>
+                    <Col lg={9} xs={10}>
+                        <InputGroup>
+                            <FormControl
+                                placeholder="Pesquisar usuário"
+                                className={styles.input}
+                                value={search}
+                                onChange={
+                                    (evt: React.ChangeEvent<HTMLInputElement>) =>
+                                        setSearch(evt.target.value)
+                                }
+
+                                onKeyPress={
+                                    (evt: React.KeyboardEvent<HTMLInputElement>) => {
+                                        if (evt.key.toLocaleLowerCase() === 'enter') {
+                                            handleSearch();
+                                        }
                                     }
                                 }
-                            }
-                    />
-                    </InputGroup>
-                </Col>
+                            />
+                        </InputGroup>
+                    </Col>
 
-                <Col lg={3} xs={2} className="mt-1" style={{ cursor: 'pointer' }}>
-                    <FontAwesomeIcon
-                        icon={faSearch}
-                        size="lg"
-                        color="var(--color-gray-light)" 
-                        className="float-left"
-                        onClick={handleSearch}
-                     />
-                </Col>
+                    <Col lg={3} xs={2} className="mt-1">
+                        <FontAwesomeIcon
+                            icon={faSearch}
+                            size="lg"
+                            color="var(--color-gray-light)"
+                            className="float-left"
+                            onClick={handleSearch}
+                        />
+                    </Col>
+                </Row>
+            </Col>
 
-                <Col lg={2} xs={{span: 3}} className={styles.titleButton}>
-                    <Link href={newPath}>
-                        <a>
-                            <StyledButton icon={icon} type_button="blue" />
-                        </a>
-                    </Link>
-                </Col>
-            </Row>
-        </Col>
+            <Col lg={2} xs={{span: 3}} className={styles.titleButton}>
+                <Link href={newPath}>
+                    <a>
+                        <StyledButton icon={icon} type_button="blue" />
+                    </a>
+                </Link>
+            </Col>
+        </Row>
     )
 }
 
--- components/shared/TitleAdminPanel/index.tsx
+++ components/shared/TitleAdminPanel/index.tsx
@@ -6,13 +6,14 @@ import styles from '../../../styles/AdminTitle.module.css';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 interface TitleAdminPanelProps {
-  title: String,
-  path: String,
-  icon?: IconProp
+  title: string;
+  path: string;
+  icon?: IconProp;
+  newPath?: string;
 }
 
 
-const TitleAdminPanel: React.FC<TitleAdminPanelProps> = ({ title, path, icon }) => {
+const TitleAdminPanel: React.FC<TitleAdminPanelProps> = ({ title, path, icon, newPath = '#' }) => {
   return (
     <Row className="mt-4">
       {
@@ -23,7 +24,7 @@ const TitleAdminPanel: React.FC<TitleAdminPanelProps> = ({ title, path, icon }) 
             </Col>
 
             <Col lg={{ span: 4, offset: 2 }} xs={8}>
-              <SearchAndIcon icon={icon} />
+              <SearchAndIcon icon={icon} newPath={newPath} />
             </Col>
           </>
           :
